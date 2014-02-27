a26e3560e6c8eb9307023c973707492cc3a992a0
Jan Dryk <jadryk@gmail.com>
2014-02-27 03:17:35 +0100

    added unit tests

---
+++ RestSys.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestSys.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
