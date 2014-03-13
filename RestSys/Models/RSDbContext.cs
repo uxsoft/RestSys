df5b9876ebb08d15cd966aeaae6fc5651fc99f9e
Jan Dryk <jadryk@gmail.com>
2014-03-13 14:48:08 +0100

    dbcontext

---
+++ RestSys/Models/RSDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestSys.Models
+{
+    public class RSDbContext : DbContext
+    {
+        public DbSet<RSDiscount> Discounts { get; set; }
+        public DbSet<RSOrder> Orders { get; set; }
+        public DbSet<RSProduct> Products { get; set; }
+        public DbSet<RSShift> Shifts { get; set; }
+        public DbSet<RSStock> Stocks { get; set; }
+        public DbSet<RSStyle> Styles { get; set; }
+        public DbSet<RSUser> Users { get; set; }    
+    
+    }
+}
