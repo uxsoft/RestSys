f60cbb3f708ab55548798aeb70004df6016470a7
Jan Dryk <jadryk@gmail.com>
2014-03-17 23:31:28 +0100

    iapiconnector

--- RestSys.Models/Exports/IRSApiConnector.cs
+++ RestSys.Models/Exports/IRSApiConnector.cs
@@ -1,23 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RestSys.Models.Exports
 {
-    public interface IRSApiConnector<T> where T : IRSEntity
+    public interface IRSApiConnector<T> : IList<T>, INotifyCollectionChanged, INotifyPropertyChanged where T : IRSEntity
     {
         IRSApiConnector<T> AttachPersistencyProvider(IRSPersistencyProvider persistencyProvider);
         IRSApiConnector<T> AttachWebApiEndpoint(string url);
 
-        Task Update(T t);
-        Task Add(T t);
-        Task Find(int id);
-        Task Delete(int id);
-        Task<ObservableCollection<T>> Get();
         Task Synchronize();
-        //or something like that
     }
 }
