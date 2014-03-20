e7c329f48d1779c1ed76df77c1a1d95cb9fc87fc
Jan Dryk <jadryk@gmail.com>
2014-03-20 21:46:20 +0100

    webservice

--- RestSys.Client/App.xaml.cs
+++ RestSys.Client/App.xaml.cs
@@ -42,6 +42,7 @@ namespace RestSys.Client
         /// <param name="e">Details about the launch request and process.</param>
         protected override async void OnLaunched(LaunchActivatedEventArgs e)
         {
+            Global.ApplicationStart();
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
---
+++ RestSys.Client/Global.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSys.Client.Services.EntityService;
+namespace RestSys.Client
+{
+    public static class Global
+    {
+        const string API_ENDPOINT = "http://localhost:56345/EntityService/";
+
+        public static void ApplicationStart()
+        {
+            Db = new RSDbContext(new Uri(API_ENDPOINT));
+        }
+
+        public static RSDbContext Db { get; set; }
+    }
+}
