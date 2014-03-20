01843eb600cc25a6870ccd80177ae12cbfaf6064
Jan Dryk <jadryk@gmail.com>
2014-03-20 15:44:49 +0100

    apiconnector

--- RestSys/App_Start/RouteConfig.cs
+++ RestSys/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Activation;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -12,12 +13,14 @@ namespace RestSys
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.Add(new ServiceRoute("EntityService", new WebServiceHostFactory(), typeof(EntityService)));
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ RestSys/EntityService.svc.cs
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using RestSys.Models;
+using System.ServiceModel;
+
+namespace RestSys
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class EntityService : EntityFrameworkDataService<EmptyDbContext>
+    {
+        protected override void HandleException(HandleExceptionArgs args)
+        {
+            base.HandleException(args);
+        }
+
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            //config.UseVerboseErrors = true;
+            //config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            //config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
+            //config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
