b4337fa2f32b67437f0e0618b26d4dd6f79c8c53
Jan Dryk <jadryk@gmail.com>
2014-04-25 15:07:06 +0200

    architecture documentation

--- RestSys.Models/Exports/IAuthenticationCookieProvider.cs
+++ RestSys.Models/Exports/IAuthenticationCookieProvider.cs
@@ -9,10 +9,34 @@ namespace RestSys.Models.Exports
 {
     public interface IAuthenticationCookieProvider
     {
+        /// <summary>
+        /// Validates the supplied credentials and creates a cookie that identifies this user
+        /// </summary>
+        /// <param name="username">username</param>
+        /// <param name="password">password</param>
+        /// <returns>If user was logged in</returns>
         bool LogIn(string username, string password);
+
+        /// <summary>
+        /// Removes users identification cookie
+        /// </summary>
         void LogOut();
+
+        /// <summary>
+        /// Indicates if the current user is logged in
+        /// </summary>
         bool IsAuthenticated { get; }
+
+        /// <summary>
+        /// Creates a principal object for the current user.
+        /// </summary>
+        /// <returns>A principal object for the current user. Returns null if no user is currently logged in.</returns>
         IPrincipal CreatePrincipal();
+
+        /// <summary>
+        /// Creates an identity object for the current user
+        /// </summary>
+        /// <returns>An identity object for the current user. Returns null if no user is currently logged in.</returns>
         IIdentity CreateIdentity();
     }
 }
--- RestSys.Models/Exports/IRSCryptographyModule.cs
+++ RestSys.Models/Exports/IRSCryptographyModule.cs
@@ -7,7 +7,18 @@ namespace RestSys.Models.Exports
 {
     public interface IRSCryptographyModule
     {
+        /// <summary>
+        /// Generates a new random salt
+        /// </summary>
+        /// <returns>random generated salt bytes</returns>
         byte[] Salt();
+        
+        /// <summary>
+        /// Hashes a password by the specified salt for secure storage
+        /// </summary>
+        /// <param name="password">password to be hashed</param>
+        /// <param name="salt">Salt value used to hash the password</param>
+        /// <returns>Hashed password</returns>
         byte[] Hash(byte[] password, byte[] salt);
     }
 }
